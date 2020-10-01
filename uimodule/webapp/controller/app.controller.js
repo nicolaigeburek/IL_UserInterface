6daf2e38064406dfc62b3b0f2ccff43ec8d5adce
nicolaigeburek <nicolai.geburek@sap.com>
2020-10-01 11:55:34 +0000

    initial commit

---
+++ uimodule/webapp/controller/app.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+  "com/sap/ILUserInterface/controller/BaseController"
+], function(Controller) {
+  "use strict";
+
+  return Controller.extend("com.sap.ILUserInterface.controller.app", {});
+});
